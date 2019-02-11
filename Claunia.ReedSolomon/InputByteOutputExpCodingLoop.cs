537cb0c27ebf64e739ecde8646b404b298197cc4
Natalia Portillo <claunia@claunia.com>
2019-02-11 01:41:10 +0000

    Add InputByteOutputExpCodingLoop class.

---
+++ Claunia.ReedSolomon/InputByteOutputExpCodingLoop.cs
@@ -0,0 +1,50 @@
+/**
+ * One specific ordering/nesting of the coding loops.
+ *
+ * Copyright 2015, Backblaze, Inc.  All rights reserved.
+ * Copyright © 2019 Natalia Portillo
+ */
+
+namespace Claunia.ReedSolomon
+{
+    public class InputByteOutputExpCodingLoop : CodingLoopBase
+    {
+        public override void CodeSomeShards(byte[][] matrixRows, byte[][] inputs, int inputCount, byte[][] outputs,
+                                            int outputCount, int offset, int byteCount)
+        {
+            {
+                int    iInput     = 0;
+                byte[] inputShard = inputs[iInput];
+
+                for(int iByte = offset; iByte < offset + byteCount; iByte++)
+                {
+                    byte inputByte = inputShard[iByte];
+
+                    for(int iOutput = 0; iOutput < outputCount; iOutput++)
+                    {
+                        byte[] outputShard = outputs[iOutput];
+                        byte[] matrixRow   = matrixRows[iOutput];
+                        outputShard[iByte] = Galois.Multiply(matrixRow[iInput], inputByte);
+                    }
+                }
+            }
+
+            for(int iInput = 1; iInput < inputCount; iInput++)
+            {
+                byte[] inputShard = inputs[iInput];
+
+                for(int iByte = offset; iByte < offset + byteCount; iByte++)
+                {
+                    byte inputByte = inputShard[iByte];
+
+                    for(int iOutput = 0; iOutput < outputCount; iOutput++)
+                    {
+                        byte[] outputShard = outputs[iOutput];
+                        byte[] matrixRow   = matrixRows[iOutput];
+                        outputShard[iByte] ^= Galois.Multiply(matrixRow[iInput], inputByte);
+                    }
+                }
+            }
+        }
+    }
+}
